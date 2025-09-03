a5d3c775195dc8eeba1eb835426086d8498f112b
Shivam Sharma <www.sharma.117@gmail.com>
2025-09-04 03:33:01 +0530

    update api

---
+++ src/main/java/com/ies/bindings/PlanForm.java
@@ -0,0 +1,47 @@
+package com.ies.bindings;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlanForm {
+
+
+    private String planCategory;
+    private String planName;
+    private LocalDate planStartDate;
+    private LocalDate planEndDate;
+	public String getPlanCategory() {
+		return planCategory;
+	}
+	public void setPlanCategory(String planCategory) {
+		this.planCategory = planCategory;
+	}
+	public String getPlanName() {
+		return planName;
+	}
+	public void setPlanName(String planName) {
+		this.planName = planName;
+	}
+	public LocalDate getPlanStartDate() {
+		return planStartDate;
+	}
+	public void setPlanStartDate(LocalDate planStartDate) {
+		this.planStartDate = planStartDate;
+	}
+	public LocalDate getPlanEndDate() {
+		return planEndDate;
+	}
+	public void setPlanEndDate(LocalDate planEndDate) {
+		this.planEndDate = planEndDate;
+	}
+    
+    
+}
---
+++ src/main/java/com/ies/entities/EligEntity.java
@@ -0,0 +1,49 @@
+package com.ies.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="ELIG_DTLS")
+public class EligEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer edgTraceId;
+
+    private String planStatus;
+
+    private Double benefitAmt;
+
+	public Integer getEdgTraceId() {
+		return edgTraceId;
+	}
+
+	public void setEdgTraceId(Integer edgTraceId) {
+		this.edgTraceId = edgTraceId;
+	}
+
+	public String getPlanStatus() {
+		return planStatus;
+	}
+
+	public void setPlanStatus(String planStatus) {
+		this.planStatus = planStatus;
+	}
+
+	public Double getBenefitAmt() {
+		return benefitAmt;
+	}
+
+	public void setBenefitAmt(Double benefitAmt) {
+		this.benefitAmt = benefitAmt;
+	}
+    
+    
+}
